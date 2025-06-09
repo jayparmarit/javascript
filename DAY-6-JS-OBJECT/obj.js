a5a747f542cf992e8c63fbe0c484aecfb8fe2db4
JAY_PARMAR <jayparmarit28@gmail.com>
2025-06-09 13:42:31 +0530

    object

---
+++ DAY-6-JS-OBJECT/obj.js
@@ -0,0 +1,79 @@
+// let obj = new object()
+// let obj = {}
+
+// obj.title = "amul milk"
+// obj.price = 50
+// obj.rate = 4.5
+
+// console.log(obj);
+// console.log(obj.title)
+
+// obj.price = 100
+
+// console.log(obj);
+
+
+// what is object // instance of the class; in which we store the data in key value pairs;
+
+// let data = {
+//     org : "sketch",
+//     found : 2009,
+//     founder : "jay parmar",
+//     "board member": "aman vats"
+// }
+
+// Object.freeze(data)
+
+// data.found = 2101;
+// data.revenue = 200000
+// console.log(data)
+
+// let newobj = Object.assign(obj, data)
+
+// console.log(newobj)
+
+// console.log(newobj.org)
+// console.log(newobj["board member"])
+// console.log(newobj["founder"])
+
+// let keys  = Object.keys(newobj)
+// let values  = Object.values(newobj)
+
+// console.log(keys)
+// console.log(values)
+
+// for(let x in newobj){
+//     console.log(x)
+//     console.log( x + ":" + newobj[x])
+// }
+
+// console.table(newobj)
+
+
+//  NESTED OBJECT 
+
+//  let obj1 = new object()
+
+//  let obj1 = {
+//     title:"amul milk",
+//     price : 45,
+//     rate:3.6
+//  }
+
+//  let obj2 = new Object();
+//  let obj2 = {}
+
+//  obj2.title = obj1.title;
+//  obj2.price = obj1.price;
+//  obj2.rate = obj1.rate;
+
+//  obj2.title = "natural milk"
+
+//  console.log(obj1)
+//  console.log(obj2)
+
+
+
+
+
+
