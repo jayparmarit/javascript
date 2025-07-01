2632cb8d25d306d628123b3743912d19b8ca3d33
JAY_PARMAR <jayparmarit28@gmail.com>
2025-07-01 12:47:45 +0530

    number complet

---
+++ DAY-15-NUMBER/script.js
@@ -0,0 +1,46 @@
+// let num = Math.random()
+// let newnum = num+1;
+// console.log(num*100000)
+
+// let n = 4.3;
+// console.log(n)
+
+// let n1 = Math.floor(n)
+// let n2 = Math.ceil(n)
+
+// console.log(n1)
+// console.log(n2)
+
+
+// let x = 7.9;
+// console.log(Math.round(x))
+
+
+// let otp = Math.round(Math.random()*1000000)
+
+// console.log(otp)
+
+
+// let d = Date.now()
+// let da = Date.toDatestring()
+// console.log(da)
+
+
+
+
+
+
+
+// let alert = document.querySelector(".alert")
+// let time = document.querySelector("#time")
+
+
+
+// function start(){
+//     setTimeout(fun, time.value*1000)
+// }
+
+// function fun(){
+//     alert.style.display = "none"
+//     console.log("time ended")
+// }
