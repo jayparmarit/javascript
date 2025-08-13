a1f279622867f85d3d7a7853d29160f1e015c88d
JAY_PARMAR <jayparmarit28@gmail.com>
2025-08-13 13:05:54 +0530

    update

--- JAVASCRIPT-REVESION-LACTURE/ARRAY/basic-operation.js
+++ JAVASCRIPT-REVESION-LACTURE/ARRAY/basic-operation.js
@@ -1,10 +1,30 @@
-let fruits = ["apple", "orange","banana","mango","strawberry","cherry"]
-console.log("fruits arry:", fruits);
+// Accessing Elements of an Array
 
-// console.log("favorite fruits", fruits[3]);
+let fruits = ["apple", "banana", "orange", "mango", "strawberry", "cherry"];
 
-// fruits[2] = "guava";
+console.log("fruits array: ", fruits);
 
-// console.log("element change", fruits[2])
+// Any element in the array can be accessed using the index number. The index in the arrays starts with 0.
 
-// console.log("fruits arry:", fruits);
+console.log("favorite fruits is ", fruits[3]);
+
+// accessing first Element of an array
+
+console.log("first fruits is", fruits[0]);
+
+// accessing last element of an array
+
+console.log("last fruits is", fruits[fruits.length - 1]);
+
+//  Modifying the Array Elements
+
+// fruits array before changing elements
+
+console.log("fruits array second element before changing is :", fruits[1]);
+
+fruits[1] = "guava";
+
+console.log(
+  "now we we have changed second element in fruits array :",
+  fruits[1]
+);
