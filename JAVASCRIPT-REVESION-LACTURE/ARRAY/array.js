526d096209c051bc28b674f9897f8915c15a878d
JAY_PARMAR <jayparmarit28@gmail.com>
2025-08-13 12:58:50 +0530

    array complete

---
+++ JAVASCRIPT-REVESION-LACTURE/ARRAY/array.js
@@ -0,0 +1,9 @@
+let fruits = ["apple", "orange", "banana", "strawberry", "guava"]
+
+console.log(typeof fruits)
+
+console.log("friuts name", fruits)
+
+let numArry = new Array(10,20,30,40,50,60)
+
+console.log("numArry", numArry)
---
+++ JAVASCRIPT-REVESION-LACTURE/ARRAY/basic-operation.js
@@ -0,0 +1,10 @@
+let fruits = ["apple", "orange","banana","mango","strawberry","cherry"]
+console.log("fruits arry:", fruits);
+
+// console.log("favorite fruits", fruits[3]);
+
+// fruits[2] = "guava";
+
+// console.log("element change", fruits[2])
+
+// console.log("fruits arry:", fruits);
