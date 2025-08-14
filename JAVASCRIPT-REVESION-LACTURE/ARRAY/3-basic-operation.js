ad78048d38373d3e146f3af530b1b9b5aa977271
JAY_PARMAR <jayparmarit28@gmail.com>
2025-08-14 13:02:37 +0530

    array complete

---
+++ JAVASCRIPT-REVESION-LACTURE/ARRAY/3-basic-operation.js
@@ -0,0 +1,31 @@
+// adding element to an array
+
+// adding element to an array in last
+
+
+const fruits = ["scorpio", "audi", "thar", "marcedise", "lambergini"]
+
+console.log(fruits.length, fruits)
+
+// <------------------!--------------->
+
+fruits.push("rolls royals")
+console.log(fruits.length, fruits)
+
+
+// <------------------!--------------->
+
+fruits.unshift("volvo")
+console.log(fruits.length, fruits)
+
+
+// <------------------!--------------->
+
+fruits.pop()
+console.log(fruits.length, fruits)
+
+
+// <------------------!--------------->
+
+fruits
+
