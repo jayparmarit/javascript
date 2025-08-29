555c4275c0cb4b6898c9ae4c243efaa941152164
JAY_PARMAR <jayparmarit28@gmail.com>
2025-08-29 13:11:36 +0530

    clearinterval complete

---
+++ JAVASCRIPT-REVESION-LACTURE/ASSYNCHRONOUS/ClearInterval.js
@@ -0,0 +1,24 @@
+// the program executes a block of code at every specified time interval. If you want to stop this function call, then you can use the clearInterval() method.
+
+// The syntax of clearInterval() method is:
+
+// clearInterval(intervalID);
+
+
+let intervalID;
+
+const currentTime = () => {
+    const Time = new Date().toLocaleTimeString();
+    console.log(Time)
+}
+
+intervalID = setInterval(currentTime, 2000)
+
+// this will run every after 2 second
+
+// now we want to stop that function execution after some time
+
+setTimeout(()=> {
+    clearInterval(intervalID)
+    console.log("stop time")
+},20000);
