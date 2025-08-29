8bb1351a384f03335402e14ea2bcbd6d1188b77f
JAY_PARMAR <jayparmarit28@gmail.com>
2025-08-29 13:10:48 +0530

    setinterval complete

---
+++ JAVASCRIPT-REVESION-LACTURE/ASSYNCHRONOUS/SetInterval.js
@@ -0,0 +1,27 @@
+ // The setInterval() method repeats a block of code at every given timing event.
+
+// The commonly used syntax of JavaScript setInterval is:
+
+// setInterval(function, milliseconds);
+// Its parameters are:
+// function - a function containing a block of code
+// milliseconds - the time interval between the execution of the function
+// The setInterval() method returns an intervalID which is a positive integer.
+
+
+function CurrentTime(){
+    let Time = new Date().toLocaleTimeString();
+
+    console.log(Time);
+}
+
+
+// now i want to execute this function every given time
+
+
+setInterval(CurrentTime, 2000);
+
+
+// In the above program, the setInterval() method calls the currentTime() function every 1000 milliseconds(1 second).
+
+// Hence the program displays the time once every 1 second.
