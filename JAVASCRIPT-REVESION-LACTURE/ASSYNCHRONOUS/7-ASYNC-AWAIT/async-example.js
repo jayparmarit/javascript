7f0d169c58b40eed8933fcbdf29bb576b017692c
JAY_PARMAR <jayparmarit28@gmail.com>
2025-09-19 11:58:07 +0530

    revesion async await

--- JAVASCRIPT-REVESION-LACTURE/ASSYNCHRONOUS/7-ASYNC-AWAIT/async-example.js
+++ JAVASCRIPT-REVESION-LACTURE/ASSYNCHRONOUS/7-ASYNC-AWAIT/async-example.js
@@ -1,4 +1,4 @@
-const api = fetch("https://jsonplaceholder.typicode.com/users");
+// const api = fetch("https://jsonplaceholder.typicode.com/users");
 
 // const users= new Promise((resolve, reject)=>{
 //     if(api.length === 0){
@@ -22,18 +22,63 @@ const api = fetch("https://jsonplaceholder.typicode.com/users");
 
 // <------------------------------------------->
 
-async function fetchData() {
-    try{
-        const res = await api;
+// async function fetchData() {
+//     try{
+//         const res = await api;
 
-        const data = await res.json();
+//         const data = await res.json();
 
-        console.log(data);
+//         console.log(data);
 
-        console.log("it will wait")
-    }catch (error){
-        console.log(error.message)
-    }
+//         console.log("it will wait")
+//     }catch (error){
+//         console.log(error.message)
+//     }
+// }
+
+// fetchData()
+
+// <-------------------------------------------->
+
+// revesion async await example
+
+const CheckPayment = () => {
+    return new Promise ((resolve, reject)=>{
+        let payment = true;
+
+        setTimeout(()=>{
+            if(payment){
+                console.log("payment done")
+                resolve();
+            }else{
+                reject("payment failed")
+            }
+        },3000);
+    })
 }
 
-fetchData()
+
+const CheckBalance = () => {
+    return new Promise ((resolve)=>{
+        setTimeout(()=>{
+            console.log("1,100,000 amount available")
+            resolve();
+        },4000)
+    })
+}
+
+const payment = async() =>{
+    try{
+        await CheckPayment();
+        console.log("fetching account balance")
+        await CheckBalance();
+    }catch(error){
+        console.log(error);
+    }finally{
+        console.log("do you want to explore more....!")
+    }
+};
+
+
+
+payment();
